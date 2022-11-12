6350caf4f68e8c889ae8e5f889c49fcd2af98111
akarah <akarahan096@gmail.com>
2022-11-12 14:45:44 +0300

    fix paths in launchd script

--- init_mac.sh
+++ init_mac.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-. ./utils.sh
+source $HOME/.dotfiles/utils.sh
 
-if check_shasum .macos.sh ~/.macos.sh; then
-	cp .macos.sh ~
-	chmod a+x ~/.macos.sh
+if check_shasum $HOME/.dotfiles/.macos.sh $HOME/.macos.sh; then
+	cp $HOME/.dotfiles/.macos.sh $HOME/.macos.sh
+	chmod a+x $HOME/.macos.sh
 fi
 
-cp com.user.loginscript.plist ~/Library/LaunchAgents/
-launchctl load ~/Library/LaunchAgents/com.user.loginscript.plist
+cp com.user.loginscript.plist $HOME/Library/LaunchAgents/
+launchctl load $HOME/Library/LaunchAgents/com.user.loginscript.plist
 #launchctl start com.user.loginscript
